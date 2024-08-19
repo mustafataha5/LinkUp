a59f19667a6425af92494418463ae9ae0c63bff6
Muath-Ademar <muazademar20@gmail.com>
2024-08-19 13:34:26 +0300

    login

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div class="navbar">
+    <div >
+        <a href="#">MyWebsite</a>
+    </div>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</div>
+  )
+}
+
+export default Header
