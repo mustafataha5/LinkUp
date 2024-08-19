87310570bdcf0976bc1a8c611e31ca798a4aadf8
mustafataha5 <mustafa.taha.mu95@gmail.com>
2024-08-19 14:04:51 +0300

    registeration backend

--- server/routers/main.router.js
+++ server/routers/main.router.js
@@ -7,7 +7,7 @@ const UserController = require('../controllers/user.controller') ;
 module.exports = app => {
     //user
     app.post("/api/register",UserController.register) ; 
-
+    app.post("/api/login",UserController.login) ; 
 
     app.post("/api/users",UserController.userCreate);
     app.get("/api/users",UserController.userFindAll);
