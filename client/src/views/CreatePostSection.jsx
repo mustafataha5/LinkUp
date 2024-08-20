a29173ed7f418e63c5ecf03aa7c941c088222ff1
Rand <r.farhoud2000@gmail.com>
2024-08-20 20:28:12 +0300

    Post section with static data version 2

---
+++ client/src/views/CreatePostSection.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Header from '../components/Header'
+import AuthorForm from '../components/AuthorForm'
+import { useState } from 'react'
+import axios from 'axios'
+import CreatePost from '../components/PostForm'
+
+const CreatePostSection = () => {
+    const [errors, setErrors] = useState([]);
+
+    // This function is used to create an author
+    const createPost = post => {
+        axios.post('http://localhost:8000/api/posts', post)
+            .then(res => {
+                console.log(res.data)
+            })
+            // If there is errors we set the errors equal to the err.response
+            // And pass it as props to the form.
+            .catch(err => {
+                console.log(err.response.data.errors)
+                const errorResponse = err.response.data.errors;
+                setErrors(errorResponse);
+            })
+    }
+    <>
+        <div className="container mt-5">
+            <CreatePost onSubmit={createPost} errors={errors} userImage={""} name={""} />
+        </div>
+    </>
+}
+
+export default CreatePostSection
--- client/src/views/MainPage.jsx
+++ client/src/views/MainPage.jsx
@@ -1,13 +1,23 @@
 import { AppBar } from '@mui/material'
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Post from '../components/Post'
 import PostList from '../components/PostList'
+import CreatePost from '../components/PostForm'
+
 const MainPage = () => {
+  const handlePostSubmit = (post) => {
+    // Handle the submitted post here (e.g., send to API, update state)
+    console.log('New post:', post);
+  };
   return (
     <div>
       <Navbar />
+      <CreatePost
+        userImage="https://example.com/user-image.jpg"
+        onPostSubmit={handlePostSubmit}
+      />
       <PostList />
+
     </div>
   )
 }
