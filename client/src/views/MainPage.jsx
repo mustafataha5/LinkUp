e8826254641dfbd67c7ea27c97dbc3592406e42f
Muath-Ademar <muazademar20@gmail.com>
2024-08-20 17:24:09 +0300

    main

--- client/src/views/MainPage.jsx
+++ client/src/views/MainPage.jsx
@@ -1,12 +1,14 @@
 import { AppBar } from '@mui/material'
 import React from 'react'
 import Navbar from '../components/Navbar'
+import FollowerSidebar from './FollowerSidebar'
+import FollowerList from './FollowerList'
 
 const MainPage = () => {
   return (
     <div>
     <Navbar/>
-      <h1>success</h1>
+      <FollowerList/>
     </div>
   )
 }
