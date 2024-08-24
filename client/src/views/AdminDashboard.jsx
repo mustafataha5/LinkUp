3814c0f317647c768bfeb9215b618e62f682fffb
mustafataha5 <mustafa.taha.mu95@gmail.com>
2024-08-24 12:46:26 +0300

    Admin route 401

--- client/src/views/AdminDashboard.jsx
+++ client/src/views/AdminDashboard.jsx
@@ -6,13 +6,35 @@ import axios from 'axios';
 import '../css/AdminStats.css';
 import AgeAnalysisBarChart from '../components/AgeAnalysisBarChart';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { useNavigate } from 'react-router-dom';
 
 const AdminDashboard = () => {
 
     const [users, setUsers] = useState([]);
     const [genderCounts, setGenderCounts] = useState({ male: 0, female: 0 });
+    const [loading,setLoading] = useState(true)
+    const navigate = useNavigate() ; 
+     // Get the user (we will get the id from the cookies then find the user)
+  useEffect(() => {
+    getUser();
+  }, []);
 
-
+  const getUser = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/api/check-auth', { withCredentials: true });
+        console.log(response.data.user)   
+      if(response.data.user.role === 'user'){
+            navigate('/401');
+           }
+    setLoading(false)    
+      
+    } catch (error) {
+      console.error('Error checking authentication', error);
+      navigate('/403'); // Redirect to login if not authenticated
+    } finally {
+      setLoading(false); // Ensure loading is false even if requests fail
+    }
+  };
     // Get all users to display them in a table in admin page
     useEffect(() => {
         getUsers();
@@ -40,7 +62,14 @@ const AdminDashboard = () => {
         setGenderCounts(counts);
     }, [users]);
 
+    if(loading){
+        return (
+            <div>
+                <AdminNavbar/>
+            </div>
 
+        )
+    }
   return (
 
     <div>
