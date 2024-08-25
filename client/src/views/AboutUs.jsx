12f022115452aaa03f7d1dc924d5e8200c025833
Muath-Ademar <muazademar20@gmail.com>
2024-08-25 11:33:53 +0300

    save

--- client/src/views/AboutUs.jsx
+++ client/src/views/AboutUs.jsx
@@ -7,26 +7,28 @@ import Mustafa from '../images/Mustafa.jpeg'
 const teamMembers = [
     {
         name: 'Rand Farhoud',
-        // position: 'CEO & Founder',
-        description: 'John is the driving force behind our vision of creating a social media platform that connects people like never before. His passion for technology and community-building leads our mission.',
+        position: 'Full Stack Developer',
+        description: `A junior full stack developer seeking an entry-level web development role, eager to refine software engineering skills and apply logical thinking to craft innovative website solutions`,
         imageUrl: '/path/to/john-doe.jpg'
     },
     {
         name: 'Mustafa Taha',
-        // position: 'Chief Technology Officer',
-        description: 'Jane oversees the technical side of our platform, ensuring that we use the latest technology to provide a seamless and innovative user experience.',
+        position: 'Full Stack Developer',
+        description: `
+
+I'm a full stack developer with expertise in Python and JavaScript, passionate about solving complex problems and delivering innovative, impactful projects.` ,
         imageUrl: Mustafa
     },
     {
         name: 'Randa Twasha',
-        // position: 'Chief Product Officer',
+        position: 'Full Stack Developer',
         description: 'Alice is responsible for product development, focusing on creating features that enhance user engagement and make our platform user-friendly and effective.',
         imageUrl: '/path/to/alice-johnson.jpg'
     },
     {
         name: 'Muath Ez Zughayyar',
-        // position: 'Chief Operations Officer',
-        description: 'Bob manages the day-to-day operations, ensuring that our platform runs smoothly and that our team delivers high-quality service to our users.',
+        position: 'Full Stack Developer',
+        description: `I'm a Junior Full Stack Developer with a Business Administration background and expertise in Java, Python, and MERN from an intensive 14-week bootcamp.`,
         imageUrl: Muath
     }
 ];
@@ -46,7 +48,14 @@ const AboutUsPage = () => {
                 <Grid container spacing={4} justifyContent="center">
                     {teamMembers.map((member, index) => (
                         <Grid item xs={12} sm={6} md={3} key={index}>
-                            <Card sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', p: 2, borderRadius: '16px', boxShadow: 3 }}>
+                            <Card sx={{ display: 'flex',
+                                 flexDirection: 'column',
+                                  alignItems: 'center',
+                                   p: 2,
+                                    borderRadius: '16px',
+                                     boxShadow: 3,
+                                     height:"400px" 
+                                     }}>
                                 <Avatar
                                     alt={member.name}
                                     src={member.imageUrl}
